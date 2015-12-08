66e77ed07ee37ad0954dda347e3cd324cc9f17de
Paul Hopkins <paul.hopkins@os.uk>
2015-12-08 13:16:07 +0000

    Initials creation

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name paulApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the paulApp
+ */
+angular.module('paulApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name paulApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the paulApp
+ */
+angular.module('paulApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
